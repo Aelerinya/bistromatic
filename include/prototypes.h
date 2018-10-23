1d0732d154c75280fbaa7650237cea09d509421f
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-23 21:51:36 +0200

    Add prototypes.h, the prototypes of all function of the bistromatic

---
+++ include/prototypes.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2018
+** prototypes
+** File description:
+** Prototypes of the functions of the bistromatic
+*/
+
+char base_to_number(char digit, char *base);
+char number_to_base(char nbr, char *base);
+char *number_str_to_array(char *str, char *base);
+void print_number_array(char *nbr_array, char *base);
+int my_strlen_nbr(char *str, char *base);
