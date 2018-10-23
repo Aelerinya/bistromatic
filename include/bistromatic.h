a7c6c08ca8a664cbded4989b44d8b74e64160254
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-23 19:56:13 +0200

    Add bistromatic.h
    Add the main header of the bistromatic. Containing the enumeration
      of the operation codes, the error messages,
      and the prototypes of the functions

---
+++ include/bistromatic.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2018
+** Bistromatic header
+** File description:
+** Header for the bistromatic with error codes, operation codes and prototypes
+*/
+
+enum {OP_OPEN_PARENT_IDX, OP_CLOSE_PARENT_IDX, OP_PLUS_IDX, OP_SUB_IDX,
+OP_NEG_IDX, OP_MULT_IDX, OP_DIV_IDX, OP_MOD_IDX};
+
+char const *SYNTAX_ERROR_MSG = "syntax error";
+char const *ERROR_MSG = "error";
+
+char *eval_expr(char const *base, char const *ops,
+char const *expr, unsigned int size);
+
+char base_to_number(char digit, char *base);
+char number_to_base(char nbr, char *base);
