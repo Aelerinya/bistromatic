a4bacbef7102f2cabe180ad34243fae6ca00f9c2
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-24 15:16:46 +0200

    infin_add.c : infinite additioner for number arrays in any base
    infin_add : allocate the memory for the result, call rec_add and manage_carry
    rec_add : add the two number arrays recursively
    manage_carry : correct the carries in substractions
    add.h : structure that contains the operands of the addition
    Added functions prototypes to prototypes.h

--- include/prototypes.h
+++ include/prototypes.h
@@ -5,9 +5,14 @@
 ** Prototypes of the functions of the bistromatic
 */
 
+#define MAX(v1, v2) (((v1) > (v2)) ? (v1) : (v2))
+#define ABS(v) (((v) > 0) ? (v) : -1 * (v))
+
 char base_to_number(char digit, char *base);
 char number_to_base(char nbr, char *base);
 char *number_str_to_array(char *str, char *base);
 void print_number_array(char *nbr_array, char *base);
 int my_strlen_nbr(char *str, char *base);
 int na_size(char *num_array);
+char *infin_add(char *str1, char *str2, char *base);
+void remove_trailing_zeros(char *nbr_array);
