a0466b092463166835a32bd56524262f27c55f28
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-29 11:56:16 +0100

    Correct result for comparision between positive and negative

--- src/infin_compare.c
+++ src/infin_compare.c
@@ -7,11 +7,24 @@
 
 #include "prototypes.h"
 
+static int verify_sign(char *nb)
+{
+    int neg;
+    
+    for (int j = 0; nb[j] != -128; j++)
+        neg = (nb[j] < 0) ? -1 : 1;
+    return neg;
+}
+
 int infin_compare(char *nb1, char *nb2)
 {
     int size1 = na_size(nb1);
     int size2 = na_size(nb2);
+    int neg1 = verify_sign(nb1);
+    int neg2 = verify_sign(nb2);
 
+    if (neg1 != neg2)
+        return (neg1);
     if (size1 > size2)
         return (1);
     else if (size1 < size2)
@@ -20,7 +33,7 @@ int infin_compare(char *nb1, char *nb2)
         if (nb1[i] > nb2[i])
             return (1);
         else if (nb1[i] < nb2[i])
-            return (-1);       
+            return (-1);
     }
     return (0);
 }
