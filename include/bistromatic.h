a00ff582bd756d49305a18616624f1bec9d70100
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-29 17:20:48 +0100

    Remove neg operator

--- include/bistromatic.h
+++ include/bistromatic.h
@@ -6,7 +6,7 @@
 */
 
 enum {OP_OPEN_PARENT_IDX, OP_CLOSE_PARENT_IDX, OP_PLUS_IDX, OP_SUB_IDX,
-OP_NEG_IDX, OP_MULT_IDX, OP_DIV_IDX, OP_MOD_IDX};
+OP_MULT_IDX, OP_DIV_IDX, OP_MOD_IDX};
 
 char const *SYNTAX_ERROR_MSG = "syntax error";
 char const *ERROR_MSG = "error";
