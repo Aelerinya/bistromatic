07355a47992e3b4fcc6e71624ce5d07de08890ff
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-29 19:16:59 +0100

    Infin_mod : modulo on number arrays
    infin_mod() is an function that just calls infin_div but returns nb1, the remainder.

---
+++ src/infin_mod.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** Infin_mod
+** File description:
+** Modulo of number arrays
+*/
+
+#include "prototypes.h"
+
+char *infin_mod(char *nb1, char *nb2, char *base)
+{
+    infin_div(nb1, nb2, base);
+    return (nb1);
+}
