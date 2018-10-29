189e29219b0d80f824f2194d8589c3f41f98f985
Augustin Gantelmi <agantelmi@localhost.localdomain>
2018-10-29 10:27:33 -0400

    Unit test fort find_starting_index

--- tests/test_find_starting_index.c
+++ tests/test_find_starting_index.c
@@ -20,17 +20,35 @@ Test(find_starting_index, positive_positive)
 
 Test(find_starting_index, negative_negative)
 {
-    char nb1[4] = {0, 2, -3, -128};
-    char nb2[3] = {2, -3, -128};
+    char nb1[4] = {0, -2, -3, -128};
+    char nb2[3] = {-2, -3, -128};
     int result = find_starting_index(nb1, nb2);
 
-    cr_assert_eq(result, 0);
+    cr_assert_eq(result, 1);
 }
 
 Test(find_starting_index, positive_negative)
 {
     char nb1[4] = {0, 4, 6, -128};
-    char nb2[4] = {2, 2, -2, -128};
+    char nb2[4] = {-2, -2, -2, -128};
+    int result = find_starting_index(nb1, nb2);
+
+    cr_assert_eq(result, 0);
+}
+
+Test(find_starting_index, negative_positive)
+{
+    char nb1[4] = {0, -2, -3, -128};
+    char nb2[4] = {2, 3, -128};
+    int result = find_starting_index(nb1, nb2);
+
+    cr_assert_eq(result, 1);
+}
+
+Test(find_starting_index, even)
+{
+    char nb1[4] = {2, 2, 2, -128};
+    char nb2[4] = {2, 2, 2, -128};
     int result = find_starting_index(nb1, nb2);
 
     cr_assert_eq(result, 0);
