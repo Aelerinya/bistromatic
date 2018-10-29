22d08cec55aedd5b085e61290638df05cdde565f
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-29 16:53:56 +0100

    infin_sub : use copy instead of number itself

--- src/infin_sub.c
+++ src/infin_sub.c
@@ -6,12 +6,19 @@
 */
 
 #include "prototypes.h"
+#include <stdlib.h>
 
 char *infin_sub(char *nb1, char *nb2, char *base)
 {
-    int size = na_size(nb2);
-    
+    int size = na_size(nb2) + 1;
+    char *tmp;
+    char *result;
+
+    if (!(tmp = malloc(sizeof(char) * size)))
+        exit(84);
     for (int i = 0; i < size; i++)
-        nb2[i] *= -1;
-    return (infin_add(nb1, nb2, base));
+        tmp[i] = -1 * nb2[i];
+    result = infin_add(nb1, tmp, base);
+    free(tmp);
+    return (result);
 }
