68465038bd6082d398b8de4fce576fda364b92ca
Lucie Philippon (Julien) <julien.philippon@epitech.eu>
2018-10-30 13:47:22 +0100

    Add disp_usage() : display program usage and exit(84)

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,17 @@
 */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include "prototypes.h"
+
+void disp_usage(void)
+{
+    write(2, "USAGE\n./calc base operators size_read\n\nDESCRIPTION\n", 51);
+    write(2, "- base: all the symbols of the base\n- operators: the ", 53);
+    write(2, "symbols for the parentheses and the 5 operators\n", 48);
+    write(2, "- size_read: number of characters to be read\n", 45);
+    exit(84);
+}
 
 void check_parenthesis(char *str, char *operators)
 {
